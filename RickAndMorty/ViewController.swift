f92ef6d50122e8816da39065bd3f61d867913e52
IC DEV <icdev@ics-mac-mini.local>
2024-11-27 15:51:37 +0700

    Initial Commit

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by IC DEV on 11/27/24.
+//  Copyright © 2024 IC DEV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
